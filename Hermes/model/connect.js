c8b1e6f1e59af7c7842fd810b909d498d2d00895
YuanFangjiang <1240229916@qq.com>
2020-06-15 14:19:11 +0800

    数据库连接

--- Hermes/model/connect.js
+++ Hermes/model/connect.js
@@ -1,6 +1,6 @@
 // 引入第三方模块
 const mongoose = require("mongoose");
 // 连接数据库
-mongoose.connect('mongodb://localhost/hermes', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://hermes:1234567@127.0.0.1/hermes', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("数据库连接成功"))
     .catch(() => console.log("数据库连接失败"))
